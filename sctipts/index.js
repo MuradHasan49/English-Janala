c8f6f15da8e49d7ba1f9292157a844a2676ab3f2
Murad Hasan <muradhasanshuco00@gmail.com>
2026-03-04 14:16:21 +0600

    fetch api and get data or show in webpage

--- sctipts/index.js
+++ sctipts/index.js
@@ -1,1 +1,19 @@
-console.log('its work')
+let datafetch = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+        .then(res => res.json())
+        .then(data => displayData(data.data))
+}
+
+let displayData = (items) => {
+    let showInHTML = document.getElementById("buttonDisplay")
+    for (let item of items){
+        console.log(item)
+        const createDiv = document.createElement("div")
+        createDiv.innerHTML=`
+       <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open-reader"></i> Lesson-${item.level_no}</button>
+        `
+        showInHTML.appendChild(createDiv)
+    }
+}
+
+datafetch()
